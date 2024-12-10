c01e24a346e03288ea3c5de7b8834399dd2f7625
Gerencserihuba <119397701+Gerencserihuba@users.noreply.github.com>
2024-12-10 08:00:27 +0100

    Update router.js

--- backend/user/router.js
+++ backend/user/router.js
@@ -1,8 +1,10 @@
 import {Router} from 'express'
 import * as user from './user-model.js'
+import auth from '../app/auth.js'
 
 const router = Router()
 
 router.post('/register',user.Register)
+router.get('/getUser',auth,user.getUserFromToken)
 
 export default router
